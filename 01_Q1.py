c5781f8472a81ad64fce0cfe6a40642f4a914464
moooooora <omarmohammed271@gmail.com>
2024-05-24 20:53:41 +0300

    Added Solve Q1

---
+++ 01_Q1.py
@@ -0,0 +1,28 @@
+# Basics 
+
+# Solo Learn ----> 3,4 core python  CS50  Easy
+# Kaggle(Python) ----> questions ------> Google    advanced
+# Kaggle(machine learning) ----> questions ------> Google    advanced
+# Kaggle(intro Sql) ----> questions ------> Google    advanced
+# Kaggle(Advanced Sql) ----> questions ------> Google    advanced
+# Kaggle(Pandas) ----> questions ------> Google    advanced
+
+# Hacker rank -----> problems ---> certified moreadvanced
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+# num %5==0
+# num > 150---->skip continue
+# num >500 ---->break 
+# # even odd
+# 11 /2
+# 5.5
+# 1
+for num in numbers:
+    if num > 500:
+        break
+    if num%5==0:
+        if num>150:
+            continue
+      
+        print(num)
+# How to write clean code
